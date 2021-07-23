c107a739bc13de4f4ac594f1578b9d8d9464f86d
bharath <bharathrb2014@gmail.com>
2021-07-23 05:35:26 +0530

    first commit

---
+++ src/test/java/firstTest/firstSelenium.java
@@ -0,0 +1,19 @@
+package firstTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class firstSelenium {
+	
+	@Test
+	public void step1() {
+		System.out.println("Hello, World!");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\Bharath Rb\\Sel Soft\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https:\\www.google.com");
+	}
+
+}
